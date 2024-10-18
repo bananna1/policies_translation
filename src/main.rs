6ad11970cc686ea5646dbcdb91807a91cb1bedf7
Anna Giacomello <108416073+bananna1@users.noreply.github.com>
2024-10-18 12:03:38 +0200

    Refined policy body. Added json serialization

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 mod parse_input_text;
 mod policy;
+use std::fs;
 
 fn main() {
-    parse_input_text::parse_input_text();
+
+    let file_path = String::from("/home/bananna/Documents/GitHub/policies_translation/src/policies.txt");
+    let output_path = String::from("/home/bananna/Documents/GitHub/policies_translation/src/policies_json.txt");
+    let policies = parse_input_text::parse_input_text(&file_path);
+    let mut  policies_string = String::new();
+    for policy in &policies {
+        let policy_json = serde_json::to_string_pretty(&policy).expect("Failed to serialize policy to json.");
+        println!("{}\n\n", policy_json);
+        policies_string = format!("{}\n\n{}", policies_string, policy_json);
+    }
+    fs::write(output_path, policies_string).expect("Unable to write to file");
+
 }
--- src/parse_input_text.rs
+++ src/parse_input_text.rs
@@ -1,18 +1,14 @@
 use std::env;
-use std::fmt::format;
 use std::fs;
 use regex::Regex;
-use crate::policy;
+use crate::policy::Policy;
 
-pub fn parse_input_text() {
+pub fn parse_input_text(file_path: &String) -> Vec<Policy> {
     let current_dir = env::current_dir().unwrap();
     println!("Current directory: {:?}", current_dir);
 
-    let file_path = String::from("/home/bananna/Documents/GitHub/policies_translation/src/policies.txt");
     let text = fs::read_to_string(file_path)
         .expect("Couldn't read file.");
-
-    //println!("{text}");
 
     // parse text in lines
     let lines = text.split("\n");
@@ -21,7 +17,7 @@ pub fn parse_input_text() {
     let subsection_re = Regex::new(r"^\s*\d+\.\s+[A-Z\-].*\s*$").unwrap();
     let paragraph_re = Regex::new(r"^\s*[a-z]\.\s+[A-Z\- ].*\s*$").unwrap();
 
-    let mut policies: Vec<policy::Policy> = Vec::new();
+    let mut policies: Vec<Policy> = Vec::new();
 
     let mut in_target_chapter = false;
 
@@ -29,34 +25,39 @@ pub fn parse_input_text() {
     let mut current_subsection_number = String::new();
     let mut current_paragraph = String::new();
     let mut current_paragraph_number = String::new();
-    let mut current_label = String::new();
 
     for line in lines {
         let trimmed_line = line.trim();
 
-        // Check if it's a chapter title and restrict to chapters 2 and 3
+        // Check if it's a chapter title
         if chapter_title_re.is_match(trimmed_line) {
             //println!("Chapter found: {}", trimmed_line);
-            in_target_chapter = true; // We are in chapters 2 or 3
+            in_target_chapter = true; // We are in chapters 2, 3 or 4
+            continue;
+        }
+
+        // Exiting target chapter
+        if trimmed_line.starts_with("CHAPTER") && !chapter_title_re.is_match(trimmed_line) {
+            in_target_chapter = false;
+            println!("{}. Exiting now", trimmed_line);
             continue;
         }
 
         // Check for a new subsection if in target chapter
         if in_target_chapter && subsection_re.is_match(trimmed_line) {
             if !current_paragraph.is_empty() {
-                let policy = build_policy_from_paragraph(&current_subsection_number, &current_paragraph_number, &current_label, &current_subsection, &current_paragraph);
+                let policy = build_policy_from_paragraph(&current_subsection_number, &current_paragraph_number, &current_subsection, &current_paragraph);
                 policies.push(policy);
                 //println!("Paragraph: {}{}\n", current_subsection_number.as_str(), current_paragraph);
             }
             if !current_subsection.is_empty() {
                 //println!("Subsection: {}\n", current_subsection);
                 if current_paragraph.is_empty() {
-                    let policy = build_policy_from_subsection(&current_subsection_number, &current_label, &current_subsection);
+                    let policy = build_policy_from_subsection(&current_subsection_number, &current_subsection);
                     policies.push(policy);
                 }
             }
             current_subsection = String::from(trimmed_line);
-            current_label = String::from(trimmed_line);
             let subsection_re_num = Regex::new(r"^\s*(\d+)\.\s+.+$").unwrap();
             current_subsection_number = String::from(subsection_re_num.captures(line).unwrap().get(1).unwrap().as_str());
 
@@ -68,7 +69,7 @@ pub fn parse_input_text() {
             // Check for a new paragraph if in target chapter
             if in_target_chapter && paragraph_re.is_match(trimmed_line) {
                 if !current_paragraph.is_empty() {
-                    let policy = build_policy_from_paragraph(&current_subsection_number, &current_paragraph_number, &current_label, &current_subsection, &current_paragraph);
+                    let policy = build_policy_from_paragraph(&current_subsection_number, &current_paragraph_number, &current_subsection, &current_paragraph);
                     policies.push(policy);
                     //println!("Paragraph: {}{}\n", current_subsection_number.as_str(), current_paragraph);
                 }
@@ -90,37 +91,34 @@ pub fn parse_input_text() {
             }
         }
 
-
-        // Exiting target chapter
-        if trimmed_line.starts_with("CHAPTER") && !chapter_title_re.is_match(trimmed_line) {
-            in_target_chapter = false;
-            println!("{}. Exiting now", trimmed_line);
-            continue;
-        }
     }
     // Output the last collected paragraph and subsection, if any
     if !current_subsection.is_empty() {
         //println!("Subsection: {}\n\n", current_subsection);
         if current_paragraph.is_empty() {
-            let policy = build_policy_from_subsection(&current_subsection_number, &current_label, &current_subsection);
+            let policy = build_policy_from_subsection(&current_subsection_number, &current_subsection);
             policies.push(policy);
         }
     }
     if !current_paragraph.is_empty() {
         if !current_paragraph.is_empty() {
-            let policy = build_policy_from_paragraph(&current_subsection_number, &current_paragraph_number, &current_label, &current_subsection, &current_paragraph);
+            let policy = build_policy_from_paragraph(&current_subsection_number, &current_paragraph_number, &current_subsection, &current_paragraph);
             policies.push(policy);
             //println!("Paragraph: {}{}\n", current_subsection_number.as_str(), current_paragraph);
         }
     }
 
-    for policy in policies {
+    /*
+    for policy in &policies {
         println!("{}\n", policy)
     }
+    */
+
+    policies
 
 }
 
-fn build_policy_from_paragraph(subsection_number: &String, paragraph_number: &String, label: &String, subsection: &String, paragraph: &String) -> policy::Policy {
+fn build_policy_from_paragraph(subsection_number: &String, paragraph_number: &String, subsection: &String, paragraph: &String) -> Policy {
     let id = format!("{}{}", subsection_number, paragraph_number);
 
     let first_line_re = Regex::new(r"^\s*\d+\.\s+(.+)$").unwrap();
@@ -137,30 +135,36 @@ fn build_policy_from_paragraph(subsection_number: &String, paragraph_number: &St
         //println!("Context:\n{}", context);
     }
 
+    // Exclude the policy id from the body
     let paragraph_re_trim = Regex::new(r"(?s)^\s*[a-z]\.\s+(.+)$").unwrap();
     let mut body = String::from(paragraph_re_trim.captures(paragraph).unwrap().get(1).unwrap().as_str());
 
-    let context_re_trim = Regex::new(r"Components shall:\s*$").unwrap();
+    // split the context into sentences
+    let sentences: Vec<&str> = context.split(". ").collect();
+    if sentences.len() > 0 {
+        let last_sentence = sentences[sentences.len() - 1];
 
-    let trim_happened = context_re_trim.is_match(&context);
+        // Check if the last sentence of the context contains the words "Component(s)" or "shall"
+        let components_re = Regex::new(r".*Component.*").unwrap();
+        let shall_re = Regex::new(r".*shall.*").unwrap();
 
-    if trim_happened {
-       context = context_re_trim.replace(&context, "").to_string();
-        let add = String::from("Components shall ");
-        body = format!("{}{}", add, body);
+        // If so, remove the last sentence from the context and add it to the body of the policy
+        if components_re.is_match(&last_sentence) || shall_re.is_match(&last_sentence) {
+            body = format!("{last_sentence} {body}");
+            context = sentences[..sentences.len() - 1].join(". ");
+        }
     }
 
-
-    let policy = policy::Policy {
-        id: id,
+    let policy = Policy {
+        id,
         label: String::from(label),
-        context: context,
-        body: body,
+        context,
+        body,
     };
     policy
 }
 
-fn build_policy_from_subsection(subsection_number: &String, label: &String, subsection: &String, ) -> policy::Policy {
+fn build_policy_from_subsection(subsection_number: &String, subsection: &String, ) -> Policy {
     let mut id = String::from(subsection_number);
 
     let first_line_re = Regex::new(r"^\s*\d+\.\s+(.+)$").unwrap();
@@ -177,11 +181,11 @@ fn build_policy_from_subsection(subsection_number: &String, label: &String, subs
         //println!("Context:\n{}", body);
     }
 
-    let policy = policy::Policy {
-        id: id,
+    let policy = Policy {
+        id,
         label: String::from(label),
         context: String::new(),
-        body: body,
+        body,
     };
     policy
 }
